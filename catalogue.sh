7a0b0f54a245f9c187ed091856746a1378bacd9a
Jayakrishna7478 <jayakrishnachowdry123@gmail.com>
2023-12-20 15:40:54 +0530

    added roboshop

--- catalogue.sh
+++ catalogue.sh
@@ -93,6 +93,6 @@ dnf install mongodb-org-shell -y &>> $LOGFILE
 
 VALIDATE $? "installing mongoDB client"
 
-mongo --host MONGODB-SERVER-IPADDRESS </app/schema/catalogue.js &>> $LOGFILE
+mongo --host mongodb.kubernete.online </app/schema/catalogue.js &>> $LOGFILE
 
 VALIDATE $? "Loading catalogue data into MongoDb"
