f53cf51533152f97daee223b8d960fcb3ae07253
Jayakrishna7478 <jayakrishnachowdry123@gmail.com>
2023-12-24 09:43:25 +0530

    added roboshop

--- catalogue.sh
+++ catalogue.sh
@@ -1,11 +1,109 @@
 #!/bin/bash
 
+# ID=$(id -u)
+# R="\e[31m"
+# G="\e[32m"
+# Y="\e[33m"
+# N="\e[0m"
+# MONGDB_HOST=mongodb.kubernete.online #give mongodb IP
+
+# TIMESTAMP=$(date +%F-%H-%M-%S)
+# LOGFILE="/tmp/$0-$TIMESTAMP.log"
+
+# echo "script stareted executing at $TIMESTAMP" &>> $LOGFILE
+
+# VALIDATE(){
+#     if [ $1 -ne 0 ]
+#     then
+#         echo -e "$2 ... $R FAILED $N"
+#         exit 1
+#     else
+#         echo -e "$2 ... $G SUCCESS $N"
+#     fi
+# }
+
+# if [ $ID -ne 0 ]
+# then
+#     echo -e "$R ERROR:: Please run this script with root access $N"
+#     exit 1 # you can give other than 0
+# else
+#     echo "You are root user"
+# fi # fi means reverse of if, indicating condition end
+
+# dnf module disable nodejs -y &>> $LOGFILE
+
+# VALIDATE $? "Disabling current NodeJS"
+
+# dnf module enable nodejs:18 -y &>> $LOGFILE
+
+# VALIDATE $? "Enableing NodeJS:18"
+
+# dnf install nodejs -y &>> $LOGFILE
+
+# VALIDATE $? "Installing NodeJS:18"
+
+# id roboshop
+# if [ $? -ne 0 ]
+# then
+#     useradd roboshop
+#     VALIDATE $? "roboshop user creation"
+# else
+#     echo -e "roboshop user already exist $Y SKIPPING $N"
+# fi
+
+# mkdir -p /app
+
+# VALIDATE $? "createing app director"
+
+# curl -o /tmp/catalogue.zip https://roboshop-builds.s3.amazonaws.com/catalogue.zip &>> $LOGFILE
+
+# VALIDATE $? "Downloading catalogue application"
+
+# cd /app
+
+# unzip -o /tmp/catalogue.zip &>> $LOGFILE
+
+# VALIDATE $? "unziping catalogue"
+
+# npm install &>> $LOGFILE
+
+# VALIDATE $? "Installing dependencies"
+
+# cp /home/centos/roboshop-shell/catalogue.service /etc/systemd/system/catalogue.service &>> $LOGFILE
+
+# VALIDATE $? "copying catalogue service file"
+
+# systemctl daemon-reload &>> $LOGFILE
+
+# VALIDATE $? "catalogue daemon reload"
+
+# systemctl enable catalogue &>> $LOGFILE
+
+# VALIDATE $? "enable catalogue"
+
+# systemctl start catalogue &>> $LOGFILE
+
+# VALIDATE $? "start catalogue"
+
+# cp /home/centos/roboshop-shell/mongo.repo /etc/yum.repos.d/mongo.repo
+
+# VALIDATE $? "installing mongo.repo"
+
+# dnf install mongodb-org-shell -y &>> $LOGFILE
+
+# VALIDATE $? "installing mongoDB client"
+
+# mongo --host $MONGDB_HOST </app/schema/catalogue.js &>> $LOGFILE
+
+# VALIDATE $? "Loading catalouge data into MongoDB"
+
+
 ID=$(id -u)
 R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
-MONGDB_HOST=mongodb.kubernete.online #give mongodb IP
+MONGDB_HOST=mongodb.kubernete.online
 
 TIMESTAMP=$(date +%F-%H-%M-%S)
 LOGFILE="/tmp/$0-$TIMESTAMP.log"
@@ -34,15 +132,15 @@ dnf module disable nodejs -y &>> $LOGFILE
 
 VALIDATE $? "Disabling current NodeJS"
 
-dnf module enable nodejs:18 -y &>> $LOGFILE
+dnf module enable nodejs:18 -y  &>> $LOGFILE
 
-VALIDATE $? "Enableing NodeJS:18"
+VALIDATE $? "Enabling NodeJS:18"
 
-dnf install nodejs -y &>> $LOGFILE
+dnf install nodejs -y  &>> $LOGFILE
 
 VALIDATE $? "Installing NodeJS:18"
 
-id roboshop
+id roboshop #if roboshop user does not exist, then it is failure
 if [ $? -ne 0 ]
 then
     useradd roboshop
@@ -53,25 +151,26 @@ fi
 
 mkdir -p /app
 
-VALIDATE $? "createing app director"
+VALIDATE $? "creating app directory"
 
-curl -o /tmp/catalogue.zip https://roboshop-builds.s3.amazonaws.com/catalogue.zip &>> $LOGFILE
+curl -o /tmp/catalogue.zip https://roboshop-builds.s3.amazonaws.com/catalogue.zip  &>> $LOGFILE
 
 VALIDATE $? "Downloading catalogue application"
 
-cd /app
+cd /app 
 
-unzip -o /tmp/catalogue.zip &>> $LOGFILE
+unzip -o /tmp/catalogue.zip  &>> $LOGFILE
 
-VALIDATE $? "unziping catalogue"
+VALIDATE $? "unzipping catalogue"
 
-npm install &>> $LOGFILE
+npm install  &>> $LOGFILE
 
 VALIDATE $? "Installing dependencies"
 
+# use absolute, because catalogue.service exists there
 cp /home/centos/roboshop-shell/catalogue.service /etc/systemd/system/catalogue.service &>> $LOGFILE
 
-VALIDATE $? "copying catalogue service file"
+VALIDATE $? "Copying catalogue service file"
 
 systemctl daemon-reload &>> $LOGFILE
 
@@ -79,20 +178,20 @@ VALIDATE $? "catalogue daemon reload"
 
 systemctl enable catalogue &>> $LOGFILE
 
-VALIDATE $? "enable catalogue"
+VALIDATE $? "Enable catalogue"
 
 systemctl start catalogue &>> $LOGFILE
 
-VALIDATE $? "start catalogue"
+VALIDATE $? "Starting catalogue"
 
 cp /home/centos/roboshop-shell/mongo.repo /etc/yum.repos.d/mongo.repo
 
-VALIDATE $? "installing mongo.repo"
+VALIDATE $? "copying mongodb repo"
 
 dnf install mongodb-org-shell -y &>> $LOGFILE
 
-VALIDATE $? "installing mongoDB client"
+VALIDATE $? "Installing MongoDB client"
 
-mongo --host mongodb.kubernete.online </app/schema/catalogue.js &>> $LOGFILE
+mongo --host $MONGDB_HOST </app/schema/catalogue.js &>> $LOGFILE
 
 VALIDATE $? "Loading catalouge data into MongoDB"
